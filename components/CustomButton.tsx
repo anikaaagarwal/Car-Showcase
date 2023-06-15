3d78befbba773bdba257241ef65d2f65f2062785
Anika Agarwal <anikaagarwal1808@gmail.com>
2023-06-15 18:10:14 +0530

    Create the navbar and footer, and also added links to the footer.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image'
 import { CustomButtonProps } from '@/types';
 
 const CustomButton = ({title, containerStyles, 
-    handleClick}: CustomButtonProps) => {
+    handleClick, btnType}: CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={'button'}
+    type={btnType || 'button'}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}>
         <span className={`flex-1`}>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x '>
         <h1 className='hero__title'>
-            Find, book, or rent a car -- quickly and easily.
+            Find, book, rent a car -- quickly and easily.
         </h1>
         <p className='hero__subtitle'>
             Streamline your car rental experience with our effortless booking process.
@@ -27,9 +27,11 @@ const Hero = () => {
       </div>
         <div className='hero__image-container'>
             <div className='hero__image'>
-                <Image src='/hero.png' alt='hero' fill className='object-contain'/>
-                <div className='hero__image-overlay'/>
+                <Image src='/hero.png' alt='hero' fill 
+                className='object-contain'/>
             </div>
+            
+            <div className='hero__image-overlay'/> 
 
         </div>
 
