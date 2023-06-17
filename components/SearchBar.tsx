d07833794c3280c35af2ca758f657da841f5aee5
Anika Agarwal <anikaagarwal1808@gmail.com>
2023-06-17 17:11:54 +0530

    Create the custom filters and append the values.

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -41,7 +41,7 @@ const SearchBar = () => {
         searchParams.delete('model')
       }
 
-      if(model){
+      if(manufacturer){
         searchParams.set('manufacturer', manufacturer)
       } else {
         searchParams.delete('manufacturer')
