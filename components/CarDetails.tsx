f8abe382535e721364dc0a9f9c4cdb15d000e330
Anika Agarwal <anikaagarwal1808@gmail.com>
2023-06-17 10:43:01 +0530

    Create the car images model using imagin studio car image api.

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Image from 'next/image';
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
+import { generateCarImageUrl } from '@/utils';
 
 interface carDetailsProps {
     isOpen: boolean;
@@ -59,7 +60,7 @@ const CarDetails = ({ isOpen, closeModal, car } : carDetailsProps) => {
                         <div className='flex-1 flex flex-col gap-3'>
                             <div className='relative w-full h-40 bg-pattern
                             bg-cover bg-center rounded-lg'>
-                                <Image src='/hero.png' alt='car model' fill 
+                                <Image src={generateCarImageUrl(car)} alt='car model' fill 
                                     priority className='object-contain'
                                 />
                             </div>
@@ -67,19 +68,19 @@ const CarDetails = ({ isOpen, closeModal, car } : carDetailsProps) => {
                             <div className='flex gap-3'>
                                 <div className='flex-1 relative w-full h-24 
                                 bg-primary-blue-100 rounded-lg'>
-                                    <Image src='/hero.png' alt='car model' fill 
+                                    <Image src={generateCarImageUrl(car , '29')} alt='car model' fill 
                                     priority className='object-contain'
                                     />
                                 </div>
                                 <div className='flex-1 relative w-full h-24 
                                 bg-primary-blue-100 rounded-lg'>
-                                    <Image src='/hero.png' alt='car model' fill 
+                                    <Image src={generateCarImageUrl(car , '33')} alt='car model' fill 
                                     priority className='object-contain'
                                     />
                                 </div>
                                 <div className='flex-1 relative w-full h-24 
                                 bg-primary-blue-100 rounded-lg'>
-                                    <Image src='/hero.png' alt='car model' fill 
+                                    <Image src={generateCarImageUrl(car , '13')} alt='car model' fill 
                                     priority className='object-contain'
                                     />
                                 </div>
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars(){
     const headers = {
 		'X-RapidAPI-Key': '25560aa804mshfc673159ee2e43fp1989ddjsn2152e965452b',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
         headers: headers,
     });
 
@@ -26,3 +28,16 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car : CarProps, angle? : string) => {
+  // customer key is : inna1
+  const url = new URL ('https://cdn.imagin.studio/getimage');
+  const {make, year, model} = car;
+  url.searchParams.append('customer', 'inna1');
+  url.searchParams.append('make',make);
+  url.searchParams.append('modelFamily',model.split(' ') [0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+  return `${url}`;
+}
