c90a3d034d4d60c0ee15b14cde52a785bc9932ea
Anika Agarwal <anikaagarwal1808@gmail.com>
2023-06-18 11:41:29 +0530

    Complete the client side rendering.

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -5,7 +5,7 @@ import { Combobox , Transition } from '@headlessui/react'
 import Image from 'next/image';
 import { manufacturers } from '@/constants';
 
-const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturerProps) => {
+const SearchManufacturer = ({selected, setSelected} : SearchManufacturerProps) => {
     const [query, setQuery] = useState('');
     const filteredManufacturers = query === '' 
     ? manufacturers 
@@ -17,7 +17,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturer
 
   return (
     <div className='search-manufacturer'>
-      <Combobox value={manufacturer} onChange={setManufacturer} >
+      <Combobox value={selected} onChange={setSelected} >
         <div className='relative w-full'>
             <Combobox.Button className='absolute top-[14px]'>
                 <Image 
